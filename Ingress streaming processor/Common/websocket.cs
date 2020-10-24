913e74e8c0416d476911cdab91c61e8402c1b3a4
Paul G Webster <paul.g.webster@googlemail.com>
2020-10-25 00:21:12 +0100

    Initial commit

---
+++ Ingress streaming processor/Common/websocket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common
+{
+    public class websocketInterface
+    {
+        private websocketContainer wsContainers = new websocketContainer();
+    }
+    public class websocketContainer
+    {
+
+    }
+}
---
+++ Ingress streaming processor/Stream conversion service CORECLR/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+using System.Configuration;
+using Common;
+
+namespace Stream_conversion_service_CORECLR
+{
+    class Program
+    {
+        // Static objects
+        static Configuration configManager = 
+            ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        static KeyValueConfigurationCollection confCollection = 
+            configManager.AppSettings.Settings;
+
+        static void Main(string[] args)
+        {
+            // Start websocket connectors - not saving packets, pass reference back to a function on main
+            websocketInterface wsCtrl = new websocketInterface();
+
+            // "wss://ws-feed.pro.coinbase.com"
+
+        }
+    }
+}
