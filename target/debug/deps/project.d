2091631e9be1e9d49de01733ade0e923500de69b
Paul G Webster <paul.g.webster@googlemail.com>
2021-04-04 23:04:45 +0100

    no idea

--- target/debug/deps/project.d
+++ target/debug/deps/project.d
@@ -1,5 +1,5 @@
-C:\Users\paulg\Documents\project\redax.dev\rust\target\debug\deps\project.exe: src\main.rs
+C:\Users\paulg\Documents\project\redax.dev\target\debug\deps\project.exe: src\main.rs
 
-C:\Users\paulg\Documents\project\redax.dev\rust\target\debug\deps\project.d: src\main.rs
+C:\Users\paulg\Documents\project\redax.dev\target\debug\deps\project.d: src\main.rs
 
 src\main.rs:
