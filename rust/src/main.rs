edbe501e728be39af61cb7efba14cf3a3f8464dc
Paul G Webster <paul.g.webster@googlemail.com>
2021-04-04 22:45:46 +0100

    Reconfiguring directory to be compatible with vscode

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let _ = gdax_websocket1_thread.join();
 }
 
-fn run_ingress_collector(gdax_websocket1_egress: std::sync::mpsc::Sender) -> () {
+fn run_ingress_collector(gdax_websocket1_egress) -> () {
     let (mut socket, response) =
         connect(Url::parse("wss://echo.websocket.org").unwrap()).expect("Can't connect");
 
