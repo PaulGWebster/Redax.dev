0e3ab4a8cb36437e4a9b053dae1e3c3924b43446
Paul G Webster <paul.g.webster@googlemail.com>
2021-04-11 10:24:37 +0000

    Added in basic TCP server thread - non integrated

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,14 @@ use url::Url;
 
 use std::collections::HashSet;
 use std::sync::mpsc::{ channel, TryRecvError };
+use std::sync::mpsc;
 use std::{thread, time};
 use std::path::Path;
+
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::BufReader;
+use std::str;
 
 use time::{Duration};
 
@@ -103,6 +109,18 @@ fn main() {
         }
     );
 
+    // Start a TCPServer
+    let (gdax_refine_ipc1_send, _gdax_refine_ipc1_recv) = channel();
+    let (gdax_refine_ipc2_send, gdax_refine_ipc2_recv) = channel();
+    let _gdax_refine_thread = thread::spawn(
+        move || {
+            run_tcp_server (
+                gdax_refine_ipc1_send,
+                gdax_refine_ipc2_recv
+            )
+        }
+    );
+
     // Main processing loop
     loop {
         // A inscope place to notify of a subscription update
@@ -169,6 +187,8 @@ fn main() {
         if flag_thread_throttle {
             thread::sleep(THROTTLE_DELAY);
         }
+
+        //
     }
 }
 
@@ -304,4 +324,73 @@ fn run_ingress_collector(
             }
         }
     };
+}
+
+fn run_tcp_server (
+    ipc_to_main: std::sync::mpsc::Sender<String>,
+    ipc_from_main: std::sync::mpsc::Receiver<String>
+) -> () {
+    println!("TCP Server thread started");
+
+    let (channel_tx, channel_rx) = mpsc::channel();
+    
+    let tx_pipe = channel_tx.clone();
+    thread::spawn(move || { listen(tx_pipe); });
+ 
+    let mut connections = 0;
+    loop{
+        match channel_rx.recv(){
+            Ok(signal)  => {
+                match signal{
+                    1 => connections += 1,
+                    2 => break,
+                    _ => println!("Invalid signal received: {}", signal)
+                }
+            }
+            Err(e)      => {
+                println!("Pipe broken - {}", e);
+            }
+        }
+    }
+}
+fn listen(tx_pipe: mpsc::Sender<u32>){
+    let listener = TcpListener::bind("127.0.0.1:7777").unwrap();
+
+    for stream in listener.incoming(){
+        match stream{
+            Ok(stream)  => {
+                println!("Received a connection! - {}:{}", stream.peer_addr().unwrap().ip(), stream.peer_addr().unwrap().port());
+                let txp = tx_pipe.clone();
+                thread::spawn(move || {
+                    connect_handler(stream, txp);
+                });
+                tx_pipe.send(1).unwrap();
+            }
+            Err(e)      => println!("Error! - {}", e)
+        }
+    }
+
+    drop(listener);
+}
+fn connect_handler(stream: TcpStream, tx_pipe: mpsc::Sender<u32>){
+    let mut buf = BufReader::new(stream.try_clone().unwrap());
+    loop{
+        //let mut s = [0u8; 4096];
+        let mut s = String::new();
+        match buf.read_line(&mut s){
+            Ok(b)  => {
+                if b == 0 { break; }
+                print!("Received data ({} bytes): {}", b, s);
+                if s.contains("quit"){
+                    tx_pipe.send(2).unwrap();
+                    break;
+                }
+                //println!("Received data ({} bytes): {}", b, std::str::from_utf8(&s).unwrap())
+            }
+            Err(e) => println!("Error receiving data! - {}", e)
+        }
+    }
+
+    println!("Client {}:{} dropped", stream.peer_addr().unwrap().ip(), stream.peer_addr().unwrap().port());
+    stream.shutdown(Shutdown::Both).unwrap();
 }
